e14b2f2b3964d118ab956c44eb243061c134e180
Sloth9731 <samweight@icloud.com>
2023-11-07 21:26:17 +0800

    asdasdas

--- floorball_voting/settings.py
+++ floorball_voting/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(b@7(#wl+j&lh!p&qo+v(!yyixty2gq$6h@w@x1scux!kwv)ms
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
